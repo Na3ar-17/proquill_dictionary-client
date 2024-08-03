5028053faf8076c582397ba867c76dd094760bf5
Na3ar-17 <gavruk2017@gmail.com>
2024-08-03 17:28:29 +0200

    installed react query and created providers.tsx

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
+
+interface IProps {
+	children: React.ReactNode
+}
+
+export function Providers({ children }: IProps) {
+	const [client] = useState(
+		new QueryClient({
+			defaultOptions: {
+				queries: {
+					refetchOnWindowFocus: false,
+				},
+			},
+		})
+	)
+
+	return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+}
