1c090b1885d5176146b13b5cf4fb32bf459cb76c
Na3ar-17 <gavruk2017@gmail.com>
2024-09-02 15:21:55 +0200

    ThemeCard.tsx: created form component

---
+++ src/components/display/Dictionary/ThemeCard/Form/FormComponent.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/components/ui/button'
+import TransparentField from '@/components/ui/custom/fields/transparent-field/TransparentField'
+import { FormField } from '@/components/ui/form'
+import { ITheme } from '@/entities/theme.entity'
+import { NextPage } from 'next'
+import { UseFormReturn } from 'react-hook-form'
+
+interface IProps {
+	form: UseFormReturn<Pick<ITheme, 'title'>, any, undefined>
+	onSubmit: () => void
+	buttonDisabled: boolean
+}
+
+const FormComponent: NextPage<IProps> = ({
+	form,
+	onSubmit,
+	buttonDisabled,
+}) => {
+	return (
+		<form onSubmit={onSubmit} className='flex items-center gap-4 mb-2'>
+			<div>
+				<FormField
+					control={form.control}
+					name='title'
+					render={({ field: { value, onChange } }) => (
+						<TransparentField value={value || ''} onChange={onChange} />
+					)}
+					rules={{
+						required: {
+							value: true,
+							message: 'This is required field',
+						},
+						minLength: {
+							value: 3,
+							message: 'At least 3 characters',
+						},
+					}}
+				/>
+				{form.formState.errors.title && (
+					<p className='text-red-500 text-sm absolute'>
+						{form.formState.errors.title.message}
+					</p>
+				)}
+			</div>
+			<Button disabled={buttonDisabled} variant={'default'}>
+				Save
+			</Button>
+		</form>
+	)
+}
+
+export default FormComponent
--- src/components/display/Dictionary/ThemeCard/ThemeCard.tsx
+++ src/components/display/Dictionary/ThemeCard/ThemeCard.tsx
@@ -1,14 +1,13 @@
-import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import BaseContextMenu from '@/components/ui/custom/context-menus/BaseContextMenu'
-import TransparentField from '@/components/ui/custom/fields/transparent-field/TransparentField'
-import { Form, FormField } from '@/components/ui/form'
+import { Form } from '@/components/ui/form'
 import { PAGES_URL } from '@/config/pages-url.config'
 import { ITheme } from '@/entities/theme.entity'
 import { dateFormatter } from '@/utils/dateFormatter'
 import { NextPage } from 'next'
 import Link from 'next/link'
 import Content from './CardContent/Content'
+import FormComponent from './Form/FormComponent'
 import { useThemeCard } from './useThemeCard'
 
 interface IProps {
@@ -42,25 +41,11 @@ const ThemeCard: NextPage<IProps> = ({
 						<CardTitle>
 							{isRename ? (
 								<Form {...form}>
-									<form
+									<FormComponent
+										buttonDisabled={updataThemeLoading}
+										form={form}
 										onSubmit={form.handleSubmit(onSubmit)}
-										className='flex items-center gap-4'
-									>
-										<FormField
-											control={form.control}
-											name='title'
-											render={({ field: { value, onChange } }) => (
-												<TransparentField
-													value={value || ''}
-													onChange={onChange}
-												/>
-											)}
-										/>
-
-										<Button disabled={updataThemeLoading} variant={'default'}>
-											Save
-										</Button>
-									</form>
+									/>
 								</Form>
 							) : (
 								<Link
