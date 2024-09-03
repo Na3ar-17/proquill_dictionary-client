6c1171c929586e5745ce513edbd212dd2de037c3
Na3ar-17 <gavruk2017@gmail.com>
2024-09-03 20:26:03 +0200

    Studying.tsx:implemented loading skeleton

--- src/components/display/Dictionary/Content/Studying/Studying.tsx
+++ src/components/display/Dictionary/Content/Studying/Studying.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import StudyingRadio from '@/components/ui/custom/radios/studying-radio/LearningRadio'
 import { Form, FormField } from '@/components/ui/form'
+import { Skeleton } from '@/components/ui/skeleton'
 import { IContentForm } from '@/types/content-form.types'
 import { htmlCleaner } from '@/utils/htmlCleaner'
 import { NextPage } from 'next'
@@ -30,41 +31,54 @@ const Studying: NextPage<IProps> = ({ id }) => {
 		<section className='flex justify-center items-center min-h-[95vh]'>
 			<Card className='w-[80%]'>
 				<CardHeader>
-					<CardTitle className='text-muted-foreground text-lg'>
+					<CardTitle className='text-muted-foreground text-base font-normal'>
 						Choose correct translation
 					</CardTitle>
 				</CardHeader>
 				<CardContent className='flex flex-col gap-12 mt-5 px-12'>
-					<div className='text-center'>
-						<p className='text-lg'>
-							{error && <span>{error.message}</span>}
-							{loading && <span>Loading</span>}
-							{htmlCleaner(data?.selectTrueTranslation.sentence || '')}
-						</p>
+					<div className='text-lg text-left text-white'>
+						{error && <span>{error.message}</span>}
+						{loading ? (
+							<Skeleton className='h-8' />
+						) : (
+							htmlCleaner(data?.selectTrueTranslation.sentence || '')
+						)}
 					</div>
 					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)}>
+						<form
+							className='flex flex-col gap-1'
+							onSubmit={form.handleSubmit(onSubmit)}
+						>
 							{error && <p>{error.message}</p>}
-							{loading && <p>Loading</p>}
-							{data?.selectTrueTranslation.variations.map((el, i) => (
-								<FormField
-									key={i}
-									control={form.control}
-									name='translation'
-									render={({ field: { onChange, value } }) => (
-										<StudyingRadio
-											onChange={() => onChange(el.translation)}
-											text={htmlCleaner(el.translation)}
-											value={el.translation}
-											selectedValue={value}
+							{loading
+								? Array.from({ length: 3 }).map((el, i) => (
+										<Skeleton className='h-[42px] mt-2 last:mt-0' key={i} />
+								  ))
+								: data?.selectTrueTranslation.variations.map((el, i) => (
+										<FormField
+											key={i}
+											control={form.control}
+											name='translation'
+											render={({ field: { onChange, value } }) => (
+												<StudyingRadio
+													onChange={() => onChange(el.translation)}
+													text={htmlCleaner(el.translation)}
+													value={el.translation}
+													selectedValue={value}
+												/>
+											)}
+											rules={{
+												required: {
+													value: true,
+													message: 'You have to choose the correct answer',
+												},
+											}}
 										/>
-									)}
-								/>
-							))}
-							<div className='flex flex-col gap-3'>
-								<div>
-									<Button>Submit</Button>
-								</div>
+								  ))}
+							<div className='mt-3'>
+								<Button disabled={!form.formState.isValid || loading}>
+									Submit
+								</Button>
 							</div>
 						</form>
 					</Form>
