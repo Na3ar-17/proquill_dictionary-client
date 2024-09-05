c52eb95ee844360e3d73e2c1d026bef4105efb81
Na3ar-17 <gavruk2017@gmail.com>
2024-09-05 08:01:15 +0200

    created hook useCookie

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,18 @@
 import SideBar from '@/components/common/side-bar/SideBar'
+import { cookies } from 'next/headers'
 import styles from './layout.module.scss'
-export default function MainLayout({
+
+export default async function MainLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
+	const isOpen = await cookies().get('isSideBarOpen')?.value
+
 	return (
 		<main className={styles.layout}>
 			<div className={styles.sidebar}>
-				<SideBar />
+				<SideBar isOpen={!!isOpen} />
 			</div>
 			<div className={styles.content}>{children}</div>
 		</main>
--- src/components/common/side-bar/SideBar.tsx
+++ src/components/common/side-bar/SideBar.tsx
@@ -1,11 +1,27 @@
 'use client'
 import { sideBarData } from '@/data/sidebar.data'
+import cookies from 'js-cookie'
+import { PanelLeftOpen, PanelRightOpen } from 'lucide-react'
 import { NextPage } from 'next'
 import Element from './Element/Element'
 import styles from './SideBar.module.scss'
-const SideBar: NextPage = () => {
+interface IProps {
+	isOpen: boolean
+}
+
+const SideBar: NextPage<IProps> = ({ isOpen }) => {
+	console.log(isOpen)
+	const handleSideBar = () => {
+		cookies.set('isSideBarOpen', isOpen ? 'false' : 'true')
+	}
+
 	return (
 		<aside className={styles.container}>
+			{isOpen ? (
+				<PanelRightOpen onClick={handleSideBar} />
+			) : (
+				<PanelLeftOpen onClick={handleSideBar} />
+			)}
 			<nav className={styles.nav}>
 				{sideBarData.map((el, i) => (
 					<Element data={el} key={i} />
