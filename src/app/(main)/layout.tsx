cabc9a14cf8868335e9532f2e87bac28b8908b46
Na3ar-17 <gavruk2017@gmail.com>
2024-09-05 13:10:57 +0200

    refactored sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,43 @@
-import { cookies } from 'next/headers'
-import ClientLayout from './clientLayout'
+'use client'
+import useCookie from '@/hooks/useCookie'
+import { sideBarCookieName, TypeIsSideBarOpen } from '@/types/sidebar.types'
+import { m } from 'framer-motion'
+import dynamic from 'next/dynamic'
+import styles from './layout.module.scss'
+
+const DynamicSideBar = dynamic(
+	() => import('@/components/common/side-bar/SideBar')
+)
 
 export default function MainLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	const cookieStore = cookies()
-	const sideBarState = cookieStore.get('isSideBarOpen')?.value
+	const [IsOpen, updateCookie] = useCookie(
+		sideBarCookieName,
+		'true' as TypeIsSideBarOpen
+	)
 
 	return (
-		<ClientLayout sideBarState={sideBarState || 'true'}>
-			{children}
-		</ClientLayout>
+		<m.main
+			className={styles.layout}
+			animate={{
+				gridTemplateColumns: IsOpen === 'false' ? '50px 1fr' : '230px 1fr',
+			}}
+			transition={{
+				type: 'spring',
+				stiffness: 300,
+				damping: 22,
+			}}
+		>
+			<div className={styles.sidebar}>
+				<DynamicSideBar
+					isOpen={IsOpen as TypeIsSideBarOpen}
+					setIsOpen={updateCookie}
+				/>
+			</div>
+			<div className={styles.content}>{children}</div>
+		</m.main>
 	)
 }
--- src/components/common/side-bar/SideBar.tsx
+++ src/components/common/side-bar/SideBar.tsx
@@ -1,41 +1,39 @@
 'use client'
 import { sideBarData } from '@/data/sidebar.data'
 import { cn } from '@/lib/utils'
+import { TypeIsSideBarOpen } from '@/types/sidebar.types'
 import { m } from 'framer-motion'
+import { PanelLeftOpen, PanelRightOpen } from 'lucide-react'
 import { NextPage } from 'next'
-import dynamic from 'next/dynamic'
+import Element from './Element/Element'
 import styles from './SideBar.module.scss'
 
-const PanelLeftOpen = dynamic(() =>
-	import('lucide-react').then(mod => mod.PanelLeftOpen)
-)
-const PanelRightOpen = dynamic(() =>
-	import('lucide-react').then(mod => mod.PanelRightOpen)
-)
-const Element = dynamic(() => import('./Element/Element'))
-
 interface IProps {
-	isOpen: string
-	setIsOpen: () => void
+	isOpen: TypeIsSideBarOpen
+	setIsOpen: (newValue: string) => void
 }
 
 const SideBar: NextPage<IProps> = ({ isOpen, setIsOpen }) => {
+	const handleSideBar = () => {
+		setIsOpen(isOpen === 'true' ? 'false' : 'true')
+	}
+
 	return (
 		<m.aside
 			animate={{ width: isOpen === 'false' ? 50 : 230 }}
 			transition={{
 				type: 'spring',
 				stiffness: 300,
-				damping: 25,
+				damping: 22,
 			}}
 			className={cn(styles.container, {
 				[styles.hidden]: isOpen === 'false',
 			})}
 		>
 			{isOpen === 'true' ? (
-				<PanelRightOpen className={styles.actionIcon} onClick={setIsOpen} />
+				<PanelRightOpen className={styles.actionIcon} onClick={handleSideBar} />
 			) : (
-				<PanelLeftOpen className={styles.actionIcon} onClick={setIsOpen} />
+				<PanelLeftOpen className={styles.actionIcon} onClick={handleSideBar} />
 			)}
 			<nav className={styles.nav}>
 				{sideBarData.map((el, i) => (
